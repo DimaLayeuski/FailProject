3c9f37e923ef0786ffc9f87d2d15fa563966626d
Dima Layeuski <dima.layeuski@gmail.com>
2022-05-27 10:50:05 +0300

    AQA-2: Create new repository test

--- NetTestFramework/NetTestFramework/Pages/MainPage.cs
+++ NetTestFramework/NetTestFramework/Pages/MainPage.cs
@@ -10,13 +10,19 @@ public class MainPage : BasePage
     private static readonly By CreateRepositoryButtonBy = By.XPath("(//*[@class='btn btn-sm btn-primary'])[1]");
     private static readonly By ChooseRepositoryButtonBy = By.XPath("(//*[@href='/DimaLayeuskiAQA/NewRepository'])[2]");
 
+    private static readonly By ChooseRenameRepositoryButtonBy =
+        By.XPath("(//*[@href='/DimaLayeuskiAQA/RenameRepository'])[2]");
+
+    public By CountOfLinkToRepositoriesBy = By.XPath("//div[@class='wb-break-word']");
+
     public IWebElement CreateRepositoryButton => WaitService.WaitElementIsExist(CreateRepositoryButtonBy);
     public IWebElement ChooseRepositoryButton => WaitService.WaitElementIsExist(ChooseRepositoryButtonBy);
-
+    public IWebElement ChooseRenameRepositoryButton => WaitService.WaitElementIsExist(ChooseRenameRepositoryButtonBy);
+    
     public MainPage(IWebDriver driver) : base(driver)
     {
     }
-    
+
     protected override By GetPageIdentifier()
     {
         return CreateRepositoryButtonBy;
